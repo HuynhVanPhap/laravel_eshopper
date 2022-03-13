c979d471e49d1d84210ed54effed2470ea8f68c3
Pháp Huỳnh <huynhvanphap198@gmail.com>
2022-03-13 17:41:36 +0700

    first commit

---
+++ app/Services/BaseService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Session;
+
+class BaseService
+{
+    public function __construct() {}
+
+    public function clearProtection(Request $request) {
+        $data = $request->except([
+            '_token',
+            'password_verify'
+        ]);
+
+        return $data;
+    }
+
+    public function setAlert($params, $messages) {
+        if (blank($params)) {
+            Session::flash('fail', $messages.strtolower(__("Fail")));
+        } else {
+            Session::flash('success', $messages.strtolower(__("Success")));
+        }
+
+        return;
+    }
+
+    public function hashPassword($user) {
+        if (isset($user['password'])) {
+            $user['password'] = Hash::make($user['password']);
+        }
+
+        return $user;
+    }
+}
